ea1df368215598bf33b7dc21fdba20918f578a03
zaincoding <105432110+zaincoding@users.noreply.github.com>
2024-02-28 21:26:33 +0500

    Assignment Complete

--- 13.Own-array/app.js
+++ 13.Own-array/app.js
@@ -1,5 +1,4 @@
 var favourite_tp = ['Bus', 'Motercyle', 'Mercedes Car', 'Bicycle'];
-var myfav_tp = "Mercedes Car";
 for (var i = 0; i < favourite_tp.length;) {
     if (favourite_tp[i] == "Mercedes Car") {
         console.log("i would like to have a", favourite_tp[i]);
