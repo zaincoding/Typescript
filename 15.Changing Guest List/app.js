df5c7e65f27c06142b75558da02832cbbc8e2e02
zaincoding <105432110+zaincoding@users.noreply.github.com>
2024-02-28 22:29:37 +0500

    Assignment Complete

--- 15.Changing Guest List/app.js
+++ 15.Changing Guest List/app.js
@@ -1,6 +1,7 @@
 var guest_list = ['Usman', 'Ahmed', 'Fawad'];
-var new_list = [];
-console.log(guest_list.toString() + ',are invited.\n');
+for (var i_1 = 0; i_1 < guest_list.length; i_1++) {
+    console.log("".concat(guest_list[i_1], ", is invited."));
+}
 console.log(guest_list[1] + ",is not participating in the invitation.");
 guest_list[1] = "Kashan";
 console.log("Now..");
