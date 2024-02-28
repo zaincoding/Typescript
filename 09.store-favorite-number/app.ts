f3ee4642cf46ebf74024473124c7eed2805a1477
zaincoding <105432110+zaincoding@users.noreply.github.com>
2024-02-28 19:14:01 +0500

    Update app.ts

--- 09.store-favorite-number/app.ts
+++ 09.store-favorite-number/app.ts
@@ -1,4 +1,4 @@
 let myFavouriteNumber: number = 98;
-let  message:string = "My faourite number is: " + 98;
+let  message:string = "My faourite number is: " + myFavouriteNumber;
 
 console.log(message);
