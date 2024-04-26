b57bf27b51d339c2b110c3766c0885a41377f866
zain <zainmd.htm@gmail.com>
2024-04-26 21:10:30 +0500

    Ramzan Coding

---
+++ NODE-PROJECTS/Atm/app.js
@@ -0,0 +1,32 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+let myBalance = 500000;
+const myPin = 1214;
+let pinAns = await inquirer.prompt([
+    { message: 'Enter your pin', name: 'pin', type: 'number',
+    }
+]);
+if (pinAns.pin === myPin) {
+    console.log('Correct pin');
+    let atmOperation = await inquirer.prompt([
+        { message: 'Select one of the option', name: 'operation', type: 'list',
+            choices: ['withdraw', 'Check Balance']
+        }
+    ]);
+    if (atmOperation.operation === 'withdraw') {
+        let withdraw = await inquirer.prompt([
+            { message: 'Enter amount', name: 'amount', type: 'list',
+                choices: ['5000', '10000', '30000']
+            }
+        ]);
+        let deduction = withdraw.amount;
+        myBalance -= deduction;
+        console.log(`Amount deducted: ${deduction}. \nYour remaining Balance: ${myBalance} PKR`);
+    }
+    if (atmOperation.operation === 'Check Balance') {
+        console.log(`Your current balance: ${myBalance} PKR`);
+    }
+}
+else {
+    console.log("Wrong pin number.");
+}
---
+++ NODE-PROJECTS/Guessing Number Game/app.ts
@@ -0,0 +1,27 @@
+#! /bin/user/env-node
+
+import inquirer  from "inquirer";
+
+async function result(){
+const randomNUmber:number = Math.round((Math.random())*5) +1;
+
+
+const answer = await inquirer.prompt([
+    {
+      message:"Enter number", type:"number",name:"Guess"
+    }
+]) as {Guess:number};
+
+const {Guess} = answer;
+
+if(Guess === randomNUmber){
+    console.log('Congratulation! You guess the correct number.')
+}
+else {
+    console.log('You guess the wrong number.', answer)
+    await result()
+}
+ }
+
+ result();
+
