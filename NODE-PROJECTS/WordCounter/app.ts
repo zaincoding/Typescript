afa323d2d29e0f1b3f8e4d9f21a7560913b64faa
zain <zainmd.htm@gmail.com>
2024-05-10 09:49:43 +0500

    Add WordCounter Project

---
+++ NODE-PROJECTS/WordCounter/app.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+let wordCounter = await inquirer.prompt([
+    {
+        message:"Enter your sentence",type:"input", name:"sentence",
+        validate: function(input:string){
+        
+        if(/^\d+$/.test(input)){
+            return "Don't enter only digits."
+        }
+        return true;
+
+        }
+    }
+]);
+
+  if(wordCounter.sentence){
+ 
+let words = wordCounter.sentence.trim().split(' ');
+if(words.length<2){
+console.log("The total word in this sentence is:", words.length);
+}else{
+  console.log("The total word in this sentence are:", words.length);
+}
+
+  }
